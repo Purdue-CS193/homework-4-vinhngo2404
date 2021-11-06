7bdeed3005480b8c7916fb3f058c9a89f0a04586
vinhngo2404 <ngo31@data.cs.purdue.edu>
2021-11-05 23:42:22 -0400

    added enum and main

--- src/Test1.java
+++ src/Test1.java
@@ -1,3 +1,11 @@
+import java.util.*;
+
+enum Operation
+{
+    ADD, SUBTRACT, MULTIPLY, DIVIDE
+}
+public class Test1 {
+
 public static double basicCalculator(Operation operation, int num1, int num2){
         double result = 0;
  if (operation == Operation.ADD) {
@@ -7,8 +15,15 @@ public static double basicCalculator(Operation operation, int num1, int num2){
         } else if (operation == Operation.MULTIPLY) {
             result = num1 * num2;
         } else if (operation == Operation.DIVIDE) {
-            result = ((double) num1) / num2;
+		if (num2 == 0) {
+			System.out.println("You can't divide by 0");
+		}
+		else result = ((double) num1) / num2;
         }
 
         return result;
        }
+public static void main(final String[] args) {
+        System.out.println(basicCalculator(Operation.DIVIDE, 1, 0));
+    }
+}
